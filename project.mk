900cf20f38ebf30f17f12e38baafe8ae2e9c493f
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-04-28 20:17:25 -0700

    initial project structure added

---
+++ project.mk
@@ -0,0 +1,17 @@
+PROJECT=jdksavdecc-proxy
+PROJECT_NAME=jdksavdecc-proxy
+PROJECT_VERSION?=201304286
+PROJECT_EMAIL=jeffk@jdkoftinoff.com
+PROJECT_LICENSE=ISC
+PROJECT_MAINTAINER=jeffk@jdkoftinoff.com
+PROJECT_COPYRIGHT=Copyright 2013
+PROJECT_DESCRIPTION=
+PROJECT_WEBSITE=https://avb.statusbar.com/jdksavdecc-proxy
+PROJECT_IDENTIFIER=org.statusbar.jdksavdecc-proxy
+TOP_LIB_DIRS+=.
+CONFIG_TOOLS+=
+PKGCONFIG_PACKAGES+=
+TEST_OUT_SUFFIX=txt
+
+CONFIG_TOOLS=
+
