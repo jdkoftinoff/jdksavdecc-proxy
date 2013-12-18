188b3ac3485bc8a22ebfff4a68719a834e8cee9c
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-12-17 22:26:08 -0800

    Better command line parsing and structure in proxyd

--- tools/posix/jdksavdecc-proxyd.c
+++ tools/posix/jdksavdecc-proxyd.c
@@ -17,14 +17,32 @@
  */
 
 #include "jdksavdecc-proxy_world.h"
-
+#include "us_daemon.h"
+#include "us_logger_syslog.h"
 #include "jdksavdecc-proxyd.h"
+#include "jdksavdecc_app.h"
 
+#ifndef JDKSAVDECC_PROXYD_IDENTITY
+#define JDKSAVDECC_PROXYD_IDENTITY "jdksavdecc-proxyd"
+#endif
+
+#ifndef JDKSAVDECC_PROXYD_CONFIG_FILE
 /**! The global configuration file */
-#define JDKSAVDECC_PROXYD_CONFIG_FILE "/etc/jdksavdecc-proxyd.conf"
+#define JDKSAVDECC_PROXYD_CONFIG_FILE "/etc/" JDKSAVDECC_PROXYD_IDENTITY ".conf"
+#endif
+
+
+/**! The printer pointing to stdout */
+us_print_file_t proxyd_stdout_printer;
+
+/**! The curret printer */
+us_print_t *proxyd_printer = 0;
 
 /**! The raw memory block used by the global allocator */
 void *proxyd_global_allocator_raw_memory = 0;
+
+/**! The malloc allocator for bootstrapping */
+us_malloc_allocator_t proxyd_bootstrap_allocator;
 
 /**! The simple allocator which uses the raw memory */
 us_simple_allocator_t proxyd_simple_allocator;
@@ -32,11 +50,29 @@ us_simple_allocator_t proxyd_simple_allocator;
 /**! The global allocator which uses the simple allocator */
 us_allocator_t *proxyd_global_allocator = 0;
 
+/**! The help option defaults off */
+bool proxyd_option_help_default=false;
+
+/**! The help option */
+bool proxyd_option_help;
+
+/**! The dump option defaults off */
+bool proxyd_option_dump_default=false;
+
+/**! The dump option */
+bool proxyd_option_dump;
+
 /**! The default log level */
-int16_t proxyd_option_log_level_default = 1;
+int16_t proxyd_option_log_level_default = US_LOG_LEVEL_INFO;
 
 /**! The actual log level */
 int16_t proxyd_option_log_level;
+
+/**! The syslog option defaults off */
+bool proxyd_option_syslog_default=false;
+
+/**! The syslog option */
+bool proxyd_option_syslog;
 
 /**! The default max static memory allocator size in K Bytes */
 uint32_t proxyd_option_memory_length_default = 32;
@@ -44,23 +80,103 @@ uint32_t proxyd_option_memory_length_default = 32;
 /**! The actual max static memory allocator size in K Bytes */
 uint32_t proxyd_option_memory_length;
 
+/**! The daemon option defaults off */
+bool proxyd_option_daemon_default=false;
+
+/**! The daemon option */
+bool proxyd_option_daemon;
+
+/**! The daemon's default identity name for logging */
+const char proxyd_option_identity_default[] = JDKSAVDECC_PROXYD_IDENTITY;
+
+/**! The daemon's identity name for logging */
+const char *proxyd_option_identity = 0;
+
+/**! The daemon's default home dir */
+const char proxyd_option_homedir_default[] = "/var/run/" JDKSAVDECC_PROXYD_IDENTITY;
+
+/**! The daemon's home dir */
+const char *proxyd_option_homedir = 0;
+
+/**! The daemon's default pidfile */
+const char proxyd_option_pidfile_default[] = "/var/run/jdksavdecc-proxyd/" JDKSAVDECC_PROXYD_IDENTITY ".pid";
+
+/**! The daemon's pidfile */
+const char *proxyd_option_pidfile = 0;
+
+/**! The daemon's default newuid */
+const char proxyd_option_newuid_default[] = "root";
+
+/**! The daemon's newuid */
+const char *proxyd_option_newuid = 0;
+
+
+/**! The default IP address to listen to */
+const char proxyd_option_address_default[] = "127.0.0.1";
+
+/**! The IP address to listen to */
+const char *proxyd_option_address = 0;
+
+/**! The default IP port to listen to */
+const char proxyd_option_port_default[] = JDKSAVDECC_APPDU_TCP_PORT_STRING;
+
+/**! The IP port to listen to */
+const char *proxyd_option_port = 0;
+
 /**! The default max per session memory allocator size in K Bytes */
 uint32_t proxyd_option_session_memory_length_default = 32;
 
 /**! The actual max per session memory allocator size in K Bytes */
 uint32_t proxyd_option_session_memory_length;
 
+
 /**! The current option collections */
-us_getopt_t proxyd_options;
+us_getopt_t proxyd_option_parser;
+
+us_getopt_option_t proxyd_global_options[]
+= {
+    {"help", "Get list of options",
+        US_GETOPT_FLAG, &proxyd_option_help_default, &proxyd_option_help },
+
+    {"dump", "Dump all current options",
+        US_GETOPT_FLAG, &proxyd_option_dump_default, &proxyd_option_dump },
+
+    {"detail", "Log detail level (0=none, 1=errors, 2=warnings, 3=info, 4=debug, 5=trace)",
+        US_GETOPT_UINT16, &proxyd_option_log_level_default, &proxyd_option_log_level},
+
+    {"syslog", "Log via syslog",
+        US_GETOPT_FLAG, &proxyd_option_syslog_default, &proxyd_option_syslog},
+
+    {"memory","Maximum memory usage in KBytes",
+        US_GETOPT_UINT32, &proxyd_option_memory_length_default, &proxyd_option_memory_length},
+
+    {"identity", "Identity",
+        US_GETOPT_STRING, &proxyd_option_identity_default, &proxyd_option_identity},
+
+    {"home", "Home directory",
+        US_GETOPT_STRING, &proxyd_option_homedir_default, &proxyd_option_homedir},
+
+    {"pidfile", "pid file name",
+        US_GETOPT_STRING, &proxyd_option_pidfile_default, &proxyd_option_pidfile},
+
+    {"newuid", "New user ID",
+        US_GETOPT_STRING, &proxyd_option_newuid_default, &proxyd_option_newuid},
+
+    {0, 0, 0, 0, 0}};
 
 /**! The options beggining with proxyd */
-us_getopt_option_t proxyd_option[]
-    = {{"detail", "Log detail level", US_GETOPT_INT16, &proxyd_option_log_level_default, &proxyd_option_log_level},
-       {"memory",                             "Maximum memory usage in KBytes", US_GETOPT_UINT32,
-        &proxyd_option_memory_length_default, &proxyd_option_memory_length},
-       {"session",                                    "Maximum session memory usage in KBytes", US_GETOPT_UINT32,
-        &proxyd_option_session_memory_length_default, &proxyd_option_session_memory_length},
-       {0, 0, 0, 0, 0}};
+us_getopt_option_t proxyd_options[]
+= {
+    {"address", "IP address to listen to ",
+        US_GETOPT_STRING, &proxyd_option_address_default, &proxyd_option_address},
+
+    {"port", "IP port to listen to ",
+        US_GETOPT_STRING, &proxyd_option_port_default, &proxyd_option_port},
+
+    {"session", "Maximum session memory usage in KBytes",
+        US_GETOPT_UINT32, &proxyd_option_session_memory_length_default, &proxyd_option_session_memory_length},
+
+    {0, 0, 0, 0, 0}};
 
 /**! Destroy the logger */
 void proxyd_destroy_logger() { us_logger_finish(); }
@@ -96,32 +212,90 @@ int proxyd_init_global_allocator() {
 }
 
 /**! Destroy the option lists */
-void proxyd_destroy_options() { us_getopt_destroy(&proxyd_options); }
+void proxyd_destroy_options() {
+    us_getopt_destroy(&proxyd_option_parser);
+}
 
 /**! Initialize the option lists, read a config file, and parse the command line */
 int proxyd_init_options(const char *config_file, const char **argv) {
-    us_getopt_init(&proxyd_options, proxyd_global_allocator);
-    us_getopt_add_list(&proxyd_options, proxyd_option, "proxyd", "JDKSAVDECC Proxy Daemon");
-    us_getopt_parse_file(&proxyd_options, config_file);
-    us_getopt_parse_args(&proxyd_options, argv);
+    int r=1;
+    /* Initialize the options manager */
+    us_getopt_init(&proxyd_option_parser, &proxyd_bootstrap_allocator.m_base);
+
+    /* Add the help and dump options with no prefix */
+    us_getopt_add_list(&proxyd_option_parser, proxyd_global_options, 0, "" );
+
+    /* Add the proxyd options with the proxyd prefix */
+    us_getopt_add_list(&proxyd_option_parser, proxyd_options, "proxyd", "JDKSAVDECC Proxy Daemon");
+
+    /* fill in the defaults and finalize the option lists */
+    us_getopt_fill_defaults(&proxyd_option_parser);
+
+    /* Try parse the global config file if any */
+    us_getopt_parse_file(&proxyd_option_parser, config_file);
+
+    /* Parse the command line arguments */
+    us_getopt_parse_args(&proxyd_option_parser, argv);
+
+    /* Remember to destroy the option list at exit */
     atexit(proxyd_destroy_options);
 
     us_log_debug("Parsed options");
 
-    return 1;
+    /* If the user is asking to dump options, then dump the values and exit */
+    if( proxyd_option_dump ) {
+        us_getopt_dump(&proxyd_option_parser, proxyd_printer, "dump" );
+        r=0;
+    }
+
+    /* If the user is asking for options, then show the options and exit */
+    if( proxyd_option_help ) {
+        us_getopt_print(&proxyd_option_parser, proxyd_printer );
+        r=0;
+    }
+
+    /* Set the logger detail level from the options */
+    us_log_level = proxyd_option_log_level;
+
+    /* re-initialize the logger to use syslog if asked to */
+    if( proxyd_option_syslog ) {
+        us_logger_syslog_start( proxyd_option_identity );
+    }
+
+    /* Initialize the global allocator now that we know how much storage to give it */
+    proxyd_init_global_allocator();
+    return r;
 }
 
 int main(int argc, const char **argv) {
     (void)argc;
     int r = 255;
 
-    if (us_platform_init_sockets() && proxyd_init_global_allocator() && proxyd_init_logger()
+    /* create a bootstrap allocator for allocations during setup */
+    us_malloc_allocator_init( &proxyd_bootstrap_allocator );
+
+    /* create a printer object tied to stdout for console access */
+    us_print_file_init(&proxyd_stdout_printer, stdout );
+    proxyd_printer = &proxyd_stdout_printer.m_base;
+
+    /* Initialize sockets, the logger, and parse the options */
+    if (us_platform_init_sockets() && proxyd_init_logger()
         && proxyd_init_options(JDKSAVDECC_PROXYD_CONFIG_FILE, argv)) {
         r = 0;
         us_log_debug("Server Starting");
 
+        /* Daemonize if necessary */
+        us_daemon_daemonize(proxyd_option_daemon,
+                            proxyd_option_identity,
+                            proxyd_option_homedir,
+                            proxyd_option_pidfile,
+                            proxyd_option_newuid);
+
+        /* Todo: Start the server */
+
         us_log_debug("Server Exiting");
     }
 
+    proxyd_bootstrap_allocator.m_base.destroy( &proxyd_bootstrap_allocator.m_base );
     return r;
 }
